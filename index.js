8adc81fbeec53a8eee1e507f20c03695fdd4e652
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2024-08-11 16:49:32 +0530

    Changes in UI

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.stayez.co.in");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
